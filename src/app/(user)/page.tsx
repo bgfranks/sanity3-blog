9007e7a6ab17f69d56d51519403b090e2caa2781
Brandon Franks <bgfranks.dev@gmail.com>
2022-12-19 21:06:56 -0500

    setup the preview mode for sanity to preview posts before publishing

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,5 +1,43 @@
-import React from 'react'
+import { previewData } from 'next/headers'
+import { groq } from 'next-sanity'
+import { client } from '../../../lib/sanity.client'
 
-export default function HomePage() {
-  return <div></div>
+// components
+import PreviewSuspense from '../../components/PreviewSuspense'
+import PreviewBlogList from '../../components/PreviewBlogList'
+
+const query = groq`
+  *[_type=='post'] {
+    ...,
+    author->,
+    categories[]->,
+  }
+    | order(_createdAt desc)
+`
+
+export default async function HomePage() {
+  if (previewData()) {
+    return (
+      <PreviewSuspense
+        fallback={
+          <div role='status'>
+            <p className='text-center text-lg animate-pulse text-[#32b9fa]'>
+              Loading Preview Data...
+            </p>
+          </div>
+        }
+      >
+        <PreviewBlogList query={query} />
+      </PreviewSuspense>
+    )
+  }
+
+  const posts = await client.fetch(query)
+
+  return (
+    <div>
+      {/* Blog List */}
+      <h1>not in preview mode</h1>
+    </div>
+  )
 }
